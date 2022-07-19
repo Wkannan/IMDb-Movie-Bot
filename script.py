65b76715ff29f8b8c7bbca50ea507c8139cd3f58
Wkannan <107666250+Wkannan@users.noreply.github.com>
2022-07-19 18:32:09 +0530

    Update script.py

--- script.py
+++ script.py
@@ -24,7 +24,7 @@ IMDb is a Open source project.
 Source: <a href='https://t.me/+CAYoHcRHqQtjZTQ1</a>
 
 <b>DEVS:</b>
-- <a href='https://t.me/josprojects'>Jos Projects</a>
+- <a href='https://t.me/wottmprojects'>wottm Projects</a>
 
 <b>SUPPORT GROUP</b>
 - <a href='https://t.me/+CAYoHcRHqQtjZTQ1'>wottm Movie Club</a>"""
